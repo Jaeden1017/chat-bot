4ba10b3fced5c34da90128e4e49aa87388b39691
Jaeden1017 <arthurj@burgate.hants.sch.uk>
2025-03-20 10:29:37 +0000

    added first question

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ print("Hello, my name is", botName)
 
 humanName = input("What is your name?")
 print("Hello", humanName)
+
+response = input("How are you today" + humanName + "(good/bad)")
+if response == "good":
+    print("I am pleased you are well")
+
+else:
+    print("I am sorry to hear you are not well?")
+    print("Have you been for a walk today?")
+    print("Or maybe gone swimming?")
+    print("Or play football?")    
